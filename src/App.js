9b00350fa591846e464c3e2f4362a52e71391eeb
CezarDev <bacanieskidev@gmail.com>
2024-07-08 14:59:40 -0300

    times criado com estilos

--- src/App.js
+++ src/App.js
@@ -4,10 +4,44 @@ import Formulario from './componentes/Fomulario';
 import Time from './componentes/Time';
 
 function App() {
-
   const times = [
-    
-  ]
+    {
+      nome: 'Cuida idoso bravo',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9',
+    },
+    {
+      nome: 'Cuida de idoso bonzinho',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+    },
+    {
+      nome: 'So cuida de mulher',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+    },
+    {
+      nome: 'So cuida de homem',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
+    },
+    {
+      nome: 'Nao cuida de problema mental',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5',
+    },
+    {
+      nome: 'So cuida de problema mental',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9',
+    },
+    {
+      nome: 'Nao da banho por nada',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+    },
+  ];
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -23,13 +57,14 @@ function App() {
           aoNovoColaboradorAdicionado(colaborador)
         }
       />
-      <Time nome='Cuida idoso bravo' />
-      <Time nome='Cuida idoso bonzinho' />
-      <Time nome='Só cuida de mulher' />
-      <Time nome='Só cuida de homen' />
-      <Time nome='Não cuida de problema mental' />
-      <Time nome='Só cuida de problema mental' />
-      <Time nome='Não da banho por nada' />
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+        />
+      ))}
     </div>
   );
 }
