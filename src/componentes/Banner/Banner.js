76c282082cb8be53e87b55639c3ed4d1dad7320f
CezarDev <bacanieskidev@gmail.com>
2024-07-09 19:58:41 -0300

    arrumando imagem barner

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css';
 export const Banner = () => {
   return (
     <header className='banner'>
-      <img src='/imagens/banner.png' alt='imagem do banner'></img>;
+      <img src='/imagens/banner.png' alt='imagem do banner'></img>
     </header>
   );
 };
